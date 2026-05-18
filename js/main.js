d7f110dc652bc10e262dd165343e5a43d9fd8174
o2taxinfo-boop <o2taxinfo@gmail.com>
2026-05-18 13:44:52 +0100

    Delete js directory

--- js/main.js
+++
@@ -1,8 +0,0 @@
-// Mobile nav toggle
-document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.nav-toggle');
-  const links = document.querySelector('.nav-links');
-  if (toggle && links) {
-    toggle.addEventListener('click', () => links.classList.toggle('open'));
-  }
-});
